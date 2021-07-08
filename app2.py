07eb3fb6937e46d4f5156a301ab5f30daeaee0ca
qualitygurus <70306234+qualitygurus@users.noreply.github.com>
2021-07-08 17:37:23 -0600

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 
+st.sidebar.<widget>
+>>> a = st.sidebar.radio('R:',[1,2])
+
 st.header('My First App')
 st.markdown('Sandeep Kumar')
 df = pd.read_csv('iris.csv')
