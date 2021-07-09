8e80a11ce150f7ba18bd804286c4a7166c130391
qualitygurus <70306234+qualitygurus@users.noreply.github.com>
2021-07-09 14:06:51 -0600

    Update WordClouds.py

--- WordClouds.py
+++ WordClouds.py
@@ -38,3 +38,11 @@ plt.imshow(wc, interpolation="bilinear")
 plt.axis('off')
 fig1 = plt.show()
 st.pyplot(fig1)
+
+
+def get_image_download_link(fig1,filename,text):
+    buffered = BytesIO()
+    img.save(buffered, format="JPEG")
+    img_str = base64.b64encode(buffered.getvalue()).decode()
+    href =  f'<a href="data:file/txt;base64,{img_str}" download="{filename}">{text}</a>'
+    return href
