fd541d6be9538a4931d17105da00ed5240c8f578
qualitygurus <70306234+qualitygurus@users.noreply.github.com>
2021-07-09 08:47:26 -0600

    Update app2.py

--- app2.py
+++ app2.py
@@ -7,7 +7,7 @@ a = st.sidebar.radio('R:',[1,2])
 st.header('My First App')
 st.markdown('Sandeep Kumar')
 df = pd.read_csv('iris.csv')
-graph = px.point(df, x= 'sepal.length', y='sepal.width') 
+graph = px.line(df, x= 'sepal.length', y='sepal.width') 
 st.plotly_charts(graph)
 
 st.write(df)
